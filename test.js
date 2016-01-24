69937b84c67f84ca58747709f3733144fff2c571
huafei117 <luhuafei123@gmail.com>
2016-01-24 10:53:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
 var express  = require('express');
-var app      = express();
+var app      = express();	
 var path = require('path');
+var numeral = require('numeral');
+var moment = require('moment');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var http = require('http');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ws = require('ws').Server;		
+var routes = require('./public/app/index.js');
+var loggerWinston = require('./public/app/logger.js');
+var block = require('./public/app/api/blocks_api');
+var tx = require('./public/app/api/transactions_api');
+var address = require('./public/app/api/address_api');
+var status = require('./public/app/api/status_api');
+var charts = require('./public/app/api/charts_api');
+var votes = require('./public/app/api/vote_api');
+var motions = require('./public/app/api/motions_api');
+var peers = require('./public/app/api/peers_api');
+var NSRaddresses = require('./public/app/api/nsr_addresses_api');
+var NBTaddresses = require('./public/app/api/nbt_addresses_api');
+var OplogWatcher = require('mongo-oplog-watcher');
+
 // configuration ===========================================
 // view engine setup
 app.set('views', path.join(__dirname, './public/app/jade'));
