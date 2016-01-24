5adce37cf44250eccad9d837e18f226122068fd1
huafei117 <luhuafei123@gmail.com>
2016-01-24 10:50:36 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+var express  = require('express');
+var app      = express();
+// configuration ===========================================
+// view engine setup
+app.set('views', path.join(__dirname, './public/app/jade'));
+app.set('view engine', 'jade');
+app.use(favicon());
+app.use(logger('dev')); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public')); // set the static files location
+app.set('port', 800);
+app.set('/',routes);
+app.get('/');
+var server = http.createServer(app).listen(app.get('port'),function() {
+    console.log("App listening on port " + app.get('port'));
+});
