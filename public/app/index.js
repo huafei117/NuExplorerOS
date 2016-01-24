f5c5a6baa8aecc21acd64f7f829a0a659d0d996d
huafei117 <luhuafei123@gmail.com>
2016-01-24 17:02:26 +0800

    Update index.js

--- public/app/index.js
+++ public/app/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-/*
+
 router.get('/', function(req, res) {
   
   res.sendfile('/jade/layout');
 
 });
-*/
+
 module.exports = function (req, res) {
   res.status(200).render('/jade/layout');
 };
